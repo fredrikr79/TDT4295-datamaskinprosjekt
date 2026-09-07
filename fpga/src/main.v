7c31ee0c3c0c488d9c532b4e698122e28ace0e0b
oyvne <nestvold03@gmail.com>
2026-09-07 14:01:06 +0200

    chore: add pipeline test-files

---
+++ fpga/src/main.v
@@ -0,0 +1,17 @@
+module main(
+    input wire clk,
+    input wire [3:0] sw,
+    output reg [3:0] led
+);
+
+    reg [26:0] counter;
+
+    always @(posedge clk) begin
+       counter <=  counter + sw;
+    
+        if (counter >= 26'd49_999_999) begin
+             counter <= 0;
+             led = led + 1'b1;
+        end
+    end
+endmodule

5a378c94d0029cfb375fee46a9ffd7349052de56
Vegard Hovland <vegard@kyrkjedelen.no>
2026-09-15 09:23:30 +0200

    Update sync params to use correct resolution

--- fpga/src/vga/vga_sync_params.sv
+++ fpga/src/vga/vga_sync_params.sv
@@ -2,20 +2,20 @@ package vga_sync_params;
   // 640X480 VGA sync parameters
 // IF CHANGING THESE, REMEMBER TO CHANGE THE PARAMETERS IN simulator.cpp AS WELL
   /* verilator lint_off UNUSEDPARAM */
-  localparam int unsigned LeftPorch = 48;
+  localparam int unsigned LeftPorch = 40;
   /* verilator lint_on UNUSEDPARAM */
-  localparam int unsigned ActiveWidth = 640;
-  localparam int unsigned RightPorch = 16;
-  localparam int unsigned HorizontalSync = 96;
-  localparam int unsigned TotalWidth = 800;
+  localparam int unsigned ActiveWidth = 854;
+  localparam int unsigned RightPorch = 8;
+  localparam int unsigned HorizontalSync = 32;
+  localparam int unsigned TotalWidth = 934;
 
-  localparam int unsigned TopPorch = 33;
+  localparam int unsigned TopPorch = 6;
   localparam int unsigned ActiveHeight = 480;
   /* verilator lint_off UNUSEDPARAM */
-  localparam int unsigned BottomPorch = 10;
+  localparam int unsigned BottomPorch = 15;
   /* verilator lint_on UNUSEDPARAM */
-  localparam int unsigned VerticalSync = 2;
-  localparam int unsigned TotalHeight = 525;
+  localparam int unsigned VerticalSync = 8;
+  localparam int unsigned TotalHeight = 509;
 
   localparam int unsigned PixelClockIncrement = 4;
 endpackage
